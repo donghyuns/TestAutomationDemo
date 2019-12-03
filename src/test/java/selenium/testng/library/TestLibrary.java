e2c0614163ef0f4ea5b35075a2ebc0c114e80e10
Donghyun Shin <donghyuns@gmail.com>
2019-12-03 17:46:25 +0900

    Add TestLibrary class

--- src/test/java/selenium/testng/library/TestLibrary.java
+++ src/test/java/selenium/testng/library/TestLibrary.java
@@ -2,19 +2,33 @@ package selenium.testng.library;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import selenium.testng.tests.TestBase;
 
 public class TestLibrary extends TestBase {
+
     public WebDriver getWebDriver(String browser) {
 
         switch (browser) {
-            case "chrome":
+            default:
                 return new ChromeDriver();
             case "ie":
-                break;
+                return new InternetExplorerDriver();
             case "ff":
-                break;
+                return new FirefoxDriver();
         }
+        /*
+        if (browser.equals("chrome"))
+            return new ChromeDriver();
+        else if (browser.equals("ie"))
+            return new InternetExplorerDriver();
+        else if (browser.equals("ff"))
+            return new FirefoxDriver();
+
+        //default
         return new ChromeDriver();
+
+         */
     }
 }
